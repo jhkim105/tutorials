ae1741d217b309d60c4046f1cb95de3ce6e7641b
jhkim105 <jhkim105@gmail.com>
2020-03-14 10:46:57 +0900

    dead letter queue

--- spring-rabbitmq-error-handling/src/main/java/com/example/demo/AmqpConfig.java
+++ spring-rabbitmq-error-handling/src/main/java/com/example/demo/AmqpConfig.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +14,29 @@ public class AmqpConfig {
 
   public static final String QUEUE_MESSAGES = "messages-queue";
   public static final String EXCHANGE_MESSAGES = "messages-exchange";
+  public static final String QUEUE_MESSAGES_DLQ = QUEUE_MESSAGES +".dlq";
+  public static final String EXCHANGE_MESSAGES_DLX = QUEUE_MESSAGES + ".dlx";
+
+  @Bean
+  FanoutExchange deadLetterExchange() {
+    return new FanoutExchange(EXCHANGE_MESSAGES_DLX);
+  }
+
+  @Bean
+  Queue deadLetterQueue() {
+    return QueueBuilder.durable(QUEUE_MESSAGES_DLQ).build();
+  }
+
+  @Bean
+  Binding dealLetterBinding() {
+    return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange());
+  }
 
   @Bean
   Queue messagesQueue() {
-    return QueueBuilder.durable(QUEUE_MESSAGES).build();
+    return QueueBuilder.durable(QUEUE_MESSAGES)
+        .withArgument("x-dead-letter-exchange", EXCHANGE_MESSAGES_DLX)
+        .build();
   }
 
   @Bean
@@ -28,5 +48,7 @@ public class AmqpConfig {
   Binding bindingMessages() {
     return BindingBuilder.bind(messagesQueue()).to(messageExchange()).with(QUEUE_MESSAGES);
   }
+
+
 
 }
--- spring-rabbitmq-error-handling/src/main/java/com/example/demo/AmqpListener.java
+++ spring-rabbitmq-error-handling/src/main/java/com/example/demo/AmqpListener.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AmqpListener {
 
-  @RabbitListener(queues = AmqpConfig.QUEUE_MESSAGES)
+  @RabbitListener(queues = AmqpConfig.QUEUE_MESSAGES, concurrency = "1")
   public void receiveMessage(Message message) {
     log.debug("received: {}", message.getPayload());
-    throw new RuntimeException("intended error");
+//    throw new RuntimeException("intended error");
+    throw new AmqpRejectAndDontRequeueException("intended error");
   }
+
+  @RabbitListener(queues = AmqpConfig.QUEUE_MESSAGES_DLQ)
+  public void processFailedMessages(Message message) {
+    log.debug("Received failed message: {}", message.toString());
+  }
+
 }
