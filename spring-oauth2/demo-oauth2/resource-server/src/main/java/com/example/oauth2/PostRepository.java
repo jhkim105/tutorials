788a6628079c41b7193d35a15c95359ed1606d06
jhkim105 <just0323>
2020-07-10 22:45:17 +0900

    .

--- spring-oauth2/demo-oauth2/resource-server/src/main/java/com/example/oauth2/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.oauth2;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
-
-}
--- spring-oauth2/spring-boot-keycloak-embedded/README.md
+++
@@ -1,8 +0,0 @@
-# Authorization Server with embedded Keycloak
-
-
-
-
-## References
-[Keycloak Embedded in a Spring Boot Application](https://www.baeldung.com/keycloak-embedded-in-spring-boot-app)
-
