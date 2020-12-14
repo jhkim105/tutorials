e8bccf2d9c78274d3f448df66fbe663c99e7653f
jhkim105 <just090323!>
2020-12-14 10:39:51 +0900

    beanio

---
+++ beanio/src/test/java/com/example/beanio/FixedLengthTest.java
@@ -0,0 +1,60 @@
+package com.example.beanio;
+
+import com.example.beanio.example.Body;
+import com.example.beanio.example.Header;
+import com.example.beanio.example.Message;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import lombok.extern.slf4j.Slf4j;
+import org.beanio.BeanReader;
+import org.beanio.BeanWriter;
+import org.beanio.StreamFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+@SpringBootTest
+@Slf4j
+public class FixedLengthTest {
+
+  @Autowired
+  private ResourceLoader resourceLoader;
+
+  @Value("${mapping-file.path}")
+  private String mappingFilePath;
+
+  @Test
+  void testFixedLength() throws IOException {
+    Message message = Message.builder()
+        .header(Header.builder()
+            .size(20)
+            .token("token")
+            .build())
+        .body(Body.builder()
+            .msg("msg")
+            .code("code")
+            .number(21)
+            .build())
+        .build();
+
+    Resource resource = resourceLoader.getResource(mappingFilePath);
+    log.info("message->{}", message);
+    StreamFactory factory = StreamFactory.newInstance();
+    factory.load(resource.getFile());
+    StringWriter stringWriter = new StringWriter();
+    BeanWriter beanWriter = factory.createWriter("message", stringWriter);
+    beanWriter.write(message);
+    String messageString = stringWriter.toString();
+    log.info("messageString->{}", messageString);
+
+
+    StringReader reader = new StringReader(messageString);
+    BeanReader beanReader = factory.createReader("message", reader);
+    Message message2 = (Message)beanReader.read();
+    log.info("{}", message2);
+  }
+}
