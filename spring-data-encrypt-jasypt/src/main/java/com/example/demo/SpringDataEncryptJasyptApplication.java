6d1c7984a1ec9d2dc70c45ef20983a220c692d1e
jhkim105 <jhkim105@gmail.com>
2021-07-10 22:10:29 +0900

    refact: rename project

---
+++ spring-data-encrypt-jasypt/src/main/java/com/example/demo/SpringDataEncryptJasyptApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataEncryptJasyptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDataEncryptJasyptApplication.class, args);
+  }
+
+}
---
+++ spring-data-initialize/src/main/java/com/example/demo/SpringDataInitializeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataInitializeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDataInitializeApplication.class, args);
+  }
+
+}
