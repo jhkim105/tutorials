4c3642c255a64c7516679dae5fd7f7cac01b6a51
jhkim105 <jhkim105@gmail.com>
2021-07-25 13:59:22 +0900

    runtime properties example with commons-configuration

--- spring-commons-configuration/src/main/java/com/example/demo/DatabasePropertiesConfig.java
+++ spring-commons-configuration/src/main/java/com/example/demo/DatabasePropertiesConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PropertiesConfig {
+public class DatabasePropertiesConfig {
 
   @Bean("databaseConfiguration")
   @ConditionalOnProperty(name = "custom.database-properties-cache.enabled", havingValue = "false")
