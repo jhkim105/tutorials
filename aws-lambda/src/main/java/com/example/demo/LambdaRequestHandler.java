62f41d59c53999ada49ff18b8934617da69b9da5
jhkim105 <jhkim105@gmail.com>
2021-07-26 12:51:19 +0900

    aws-lambda example

---
+++ aws-lambda/src/main/java/com/example/demo/LambdaRequestHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaRequestHandler implements RequestHandler<String, String> {
+
+  @Override
+  public String handleRequest(String input, Context context) {
+    context.getLogger().log("Input:" + input);
+
+    return "Hello Lambda - " + input;
+  }
+
+}
---
+++ aws-lambda/src/test/java/com/example/demo/LambdaRequestHandlerTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LambdaRequestHandlerTest {
+
+  @Test
+  void test() {
+    System.out.println("aaaa");
+
+    Context ctx = new TestContext();
+
+    LambdaRequestHandler handler = new LambdaRequestHandler();
+    String output = handler.handleRequest("Test", ctx);
+    Assertions.assertTrue(output.startsWith("Hello"));
+  }
+
+}
