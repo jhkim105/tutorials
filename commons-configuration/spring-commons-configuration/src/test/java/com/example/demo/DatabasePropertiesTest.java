781b9990d348e1123c1f40a19fcabdbf47902d8e
jhkim105 <jhkim105@gmail.com>
2021-07-29 22:32:01 +0900

    refact:

---
+++ commons-configuration/spring-commons-configuration/src/test/java/com/example/demo/DatabasePropertiesTest.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.configuration.DatabaseConfiguration;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class DatabasePropertiesTest {
+
+  @Autowired
+  private DatabaseConfiguration databaseConfiguration;
+
+
+  @Test
+  void getProperty() {
+    databaseConfiguration.getProperty("prop1");
+    databaseConfiguration.getProperty("prop1");
+    databaseConfiguration.getProperty("prop2");
+    databaseConfiguration.addProperty("prop1", "1");
+    Assertions.assertThat(databaseConfiguration.getProperty("prop1")).isEqualTo("1");
+    databaseConfiguration.getProperty("prop2");
+    databaseConfiguration.setProperty("prop1", "2");
+    Assertions.assertThat(databaseConfiguration.getProperty("prop1")).isEqualTo("2");
+    databaseConfiguration.getProperty("prop2");
+  }
+
+  @Test
+  void getString() {
+    databaseConfiguration.getProperty("prop1");
+    databaseConfiguration.getProperty("prop1");
+    databaseConfiguration.getString("prop1"); // Not applied cache: self-invocation
+    databaseConfiguration.getString("prop1"); // Not applied cache: self-invocation
+  }
+
+}
---
+++ commons-configuration/spring-commons-configuration2/src/test/java/com/example/demo/RuntimePropertiesTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import javax.annotation.Resource;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class RuntimePropertiesTest {
+
+  @Resource(name = "runtimePropertiesBuilder")
+  private ReloadingFileBasedConfigurationBuilder runtimePropertiesBuilder;
+
+  @SneakyThrows
+  @Test
+  void test() {
+    Assertions.assertThat(runtimePropertiesBuilder.getConfiguration().getString("prop1")).isEqualTo("a");
+    Assertions.assertThat(runtimePropertiesBuilder.getConfiguration().getString("prop2")).isEqualTo("b");
+  }
+}
