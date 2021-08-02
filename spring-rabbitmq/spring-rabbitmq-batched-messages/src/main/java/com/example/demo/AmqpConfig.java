cf70f141e02c1988015582b5e30ad7fd3fe29cd2
jhkim105 <jhkim105@gmail.com>
2021-08-02 13:43:03 +0900

    rabbmitmq batched message

--- spring-rabbitmq/spring-rabbitmq-batched-messages/src/main/java/com/example/demo/AmqpConfig.java
+++ spring-rabbitmq/spring-rabbitmq-batched-messages/src/main/java/com/example/demo/AmqpConfig.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@Slf4j
 public class AmqpConfig {
 
   static final String QUEUE_A = "queue-a";
@@ -30,5 +35,22 @@ public class AmqpConfig {
 //    container.setDeBatchingEnabled(true);
     return container;
   }
+
+
+  @Bean
+  public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setBatchListener(true);
+    factory.setConsumerBatchEnabled(true);
+    factory.setBatchSize(10);
+    return factory;
+  }
+
+  @RabbitListener(queues = QUEUE_A)
+  public void listen(List<String> list) {
+    log.info("messages->{}", list);
+  }
+
 
 }
