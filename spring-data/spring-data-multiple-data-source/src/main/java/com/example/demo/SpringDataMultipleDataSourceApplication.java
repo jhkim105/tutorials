2a4ecc0fa1f465afc2b64de1fc16333f81a512f1
jhkim105 <jhkim105@gmail.com>
2021-08-09 20:52:23 +0900

    .

--- spring-data/spring-data-multiple-data-source/src/main/java/com/example/demo/SpringDataMultipleDataSourceApplication.java
+++ spring-data/spring-data-multiple-data-source/src/main/java/com/example/demo/SpringDataMultipleDataSourceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringDataMultipleDataSourceApplication {
 
