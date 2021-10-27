c7e999f823971c19f789ab904b23e7f7e56c4b2b
jhkim105 <jhkim105@gmail.com>
2021-10-27 11:00:27 +0900

    spring-cloud-config: refresh
    spring-cloud-config: refresh
    
    spring-cloud-config
    
    spring-cloud-config

--- spring-cloud-config/config-client/src/main/java/jhkim105/tutorials/config/client/HomeController.java
+++ spring-cloud-config/config-client/src/main/java/jhkim105/tutorials/config/client/HomeController.java
@@ -1,10 +1,12 @@
 package jhkim105.tutorials.config.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HomeController {
 
   @Value("${config.name}")
