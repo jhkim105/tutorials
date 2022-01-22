138b230c01128a802d50d047ad1c0ad0d292d127
jhkim105 <jhkim105@gmail.com>
2022-01-22 09:42:57 +0900

    chore:

--- persistence/spring-data-jpa/src/test/java/jhkim105/tutorials/spring/data/jpa/SpringDataJpaApplicationTests.java
+++ persistence/spring-data-jpa/src/test/java/jhkim105/tutorials/spring/data/jpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,26 @@
 package jhkim105.tutorials.spring.data.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.jpa.EntityManagerFactoryInfo;
 
 @SpringBootTest
+@Slf4j
 class SpringDataJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Test
+	void test() {
+		log.debug("{}", entityManager);
+		log.debug("{}", ((EntityManagerFactoryInfo)entityManager.getEntityManagerFactory()).getPersistenceUnitName());
+	}
 }
