1bb547aeaf2320326b28e93c622ed6bf8bbf6055
jhkim105 <jhkim105@gmail.com>
2022-03-30 00:33:59 +0900

    spring cloud config encryption

--- spring-cloud/spring-cloud-config/config-client/src/main/java/jhkim105/tutorials/config/client/ConfigClientApplication.java
+++ spring-cloud/spring-cloud-config/config-client/src/main/java/jhkim105/tutorials/config/client/ConfigClientApplication.java
@@ -1,13 +1,27 @@
 package jhkim105.tutorials.config.client;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class ConfigClientApplication {
+@RequiredArgsConstructor
+@Slf4j
+public class ConfigClientApplication implements ApplicationRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(ConfigClientApplication.class, args);
   }
 
+  private final Environment environment;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    log.info("config.name:{}", environment.getProperty("config.name"));
+    log.info("config.password:{}", environment.getProperty("config.password"));
+  }
 }
