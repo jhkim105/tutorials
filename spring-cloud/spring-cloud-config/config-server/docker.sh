51b9da0c8823e93b705b5af51832e1c4c6a96acf
jhkim105 <jhkim105@gmail.com>
2022-10-24 17:23:30 +0900

    chore:

---
+++ spring-cloud/spring-cloud-config/config-server/docker.sh
@@ -0,0 +1,9 @@
+docker run --rm -it -p 8888:8888 -m=2g \
+      --platform linux/amd64 \
+      -v /Users/jihwankim/dev/my/tutorials/spring-cloud/spring-cloud-config/config-server/config:/config \
+      -e SPRING_PROFILES_ACTIVE=native,cloud-bus-rabbit  \
+      -e SPRING_RABBITMQ_HOST=host.docker.internal \
+      -e SPRING_RABBITMQ_USERNAME=guest \
+      -e SPRING_RABBITMQ_PASSWORD=guest \
+      -e SPRING_RABBITMQ_VIRTUAL_HOST=rmlocal \
+      hyness/spring-cloud-config-server
