c859c0461b9b270f67ad2c7d433efa64c8881001
jhkim105 <jhkim105@gmail.com>
2022-10-25 20:08:19 +0900

    @RecordApplicationEvents

--- spring-events/src/test/java/jhkim105/tutorials/spring/events/simple/CustomSpringEventTest.java
+++ spring-events/src/test/java/jhkim105/tutorials/spring/events/simple/CustomSpringEventTest.java
@@ -1,16 +1,22 @@
 package jhkim105.tutorials.spring.events.simple;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
 
 
 @SpringBootTest
+@RecordApplicationEvents
 @Slf4j
 class CustomSpringEventTest {
 
+  @Autowired
+  private ApplicationEvents applicationEvents;
   @Autowired
   private CustomSpringEventPublisher publisher;
 
@@ -18,6 +24,8 @@ class CustomSpringEventTest {
   void event() {
     publisher.publishCustomEvent("Hello world!!");
     log.info("Done publishing synchronous custom event.");
+    assertThat(applicationEvents.stream(CustomSpringEvent.class).count()).isEqualTo(1);
+
   }
 
 }
