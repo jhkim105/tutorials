71c1a84b996d86db2bca15e30e6e80ba1b6d03cd
jhkim105 <jhkim105@gmail.com>
2022-11-01 14:14:00 +0900

    spring security dsl

--- security/spring-security/spring-security-form-login/src/main/java/jhkim105/tutorials/spring/security/form_login/config/SecurityConfig.java
+++ security/spring-security/spring-security-form-login/src/main/java/jhkim105/tutorials/spring/security/form_login/config/SecurityConfig.java
@@ -5,34 +5,28 @@ import jhkim105.tutorials.spring.security.form_login.security.UserDetailsService
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
     http
-        .authorizeRequests()
-            .antMatchers("/", "/home").permitAll()
-            .anyRequest().authenticated()
-            .and()
-        .formLogin()
-//            .loginPage("/login")
-            .permitAll()
-            .and()
-        .logout()
-            .permitAll()
-            .and()
-        .exceptionHandling()
-          .authenticationEntryPoint(new CustomAuthenticationEntryPoint("/login"));
+        .authorizeHttpRequests((authz) -> authz
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout(LogoutConfigurer::permitAll)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint("/login"));
+    return http.build();
   }
 
   @Bean
-  @Override
   protected UserDetailsService userDetailsService() {
     return new UserDetailsServiceImpl();
   }
