647dd3874d3c5839bb2ece4296891bcfc97646b9
jhkim105 <jhkim105@gmail.com>
2023-04-10 20:21:30 +0900

    .

--- security/oauth2-client/oauth2-keycloak/src/main/java/jhkim105/tutorials/oauth2/keycloak/HomeController.java
+++ security/oauth2-client/oauth2-keycloak/src/main/java/jhkim105/tutorials/oauth2/keycloak/HomeController.java
@@ -3,21 +3,14 @@ package jhkim105.tutorials.oauth2.keycloak;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
 public class HomeController {
-  @GetMapping("/home")
+  @GetMapping("/")
   public String home() {
     log.info("/home");
     return "home";
   }
 
-  @GetMapping("/")
-  @ResponseBody
-  public String root() {
-    log.info("/");
-    return "This is Index Page";
-  }
 }
--- security/oauth2-client/oauth2-keycloak/src/main/java/jhkim105/tutorials/oauth2/keycloak/SecurityConfig.java
+++ security/oauth2-client/oauth2-keycloak/src/main/java/jhkim105/tutorials/oauth2/keycloak/SecurityConfig.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -16,8 +18,9 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
     http
         .authorizeHttpRequests(auth -> auth
-                .antMatchers("/user-info").hasRole("USER")
-                .anyRequest().permitAll());
+            .antMatchers("/login").permitAll()
+            .anyRequest().authenticated());
+
     http.oauth2Login();
     http.logout(logout -> logout.addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/"));
     return http.build();
