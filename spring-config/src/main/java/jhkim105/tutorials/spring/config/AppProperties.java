d5768cb3979c4193fe96814db318f4b88556385e
jhkim105 <jhkim105@gmail.com>
2023-12-21 22:15:23 +0900

    @NestedConfigurationProperty

--- spring-config/src/main/java/jhkim105/tutorials/spring/config/AppProperties.java
+++ spring-config/src/main/java/jhkim105/tutorials/spring/config/AppProperties.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,4 +17,16 @@ public class AppProperties {
   private String name;
   private String storagePath;
 
+  private InnerProperties inner;
+
+
+  @NestedConfigurationProperty // 외부에 있는 클래스 사용시 필요. 없어도 값은 할당되지만 application.yml 에서 Cannot resolve configuration property
+  private SampleProperties sample;
+
+  @Getter
+  @Setter
+  @ToString
+  public static class InnerProperties {
+    private String name;
+  }
 }
--- spring-config/src/test/java/jhkim105/tutorials/spring/config/SpringConfigApplicationTests.java
+++ spring-config/src/test/java/jhkim105/tutorials/spring/config/SpringConfigApplicationTests.java
@@ -1,13 +1,20 @@
 package jhkim105.tutorials.spring.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringConfigApplicationTests {
+
+	@Autowired
+	private AppProperties appProperties;
 
 	@Test
 	void contextLoads() {
+		log.info("{}", appProperties);
 	}
 
 }
