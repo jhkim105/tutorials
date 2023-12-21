977c1f97a1efb8eeacc6401841f3fea9de2335b6
jhkim105 <jhkim105@gmail.com>
2023-12-21 22:31:37 +0900

    aws profile

---
+++ aws/spring-cloud-aws/src/test/java/jhkim105/tutorials/spring_cloud_aws/s3/S3Tests.java
@@ -0,0 +1,29 @@
+package jhkim105.tutorials.spring_cloud_aws.s3;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+@SpringBootTest
+@Slf4j
+class S3Tests {
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+
+
+    @Test
+    void testResource() {
+        String objectUrl = "s3://rtm-test/1.txt";
+        Resource resource = resourceLoader.getResource(objectUrl);
+        log.debug("{}", resource.exists());
+    }
+
+
+
+}
