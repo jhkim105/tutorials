72c1370119fe1b2553985ece819faa7d0f1a2ca3
jhkim105 <jhkim105@gmail.com>
2024-01-29 23:16:10 +0900

    mimetype

--- core-java/core-java-io/src/test/java/jhkim105/tutorials/io/MimeTypeTests.java
+++ core-java/core-java-io/src/test/java/jhkim105/tutorials/io/MimeTypeTests.java
@@ -13,15 +13,15 @@ import org.junit.jupiter.api.Test;
 /**
  * Tika: 파일 메타데이터로 mimeType 결정
  * Files.probeContentType(): 파일 확장자로 mimeType 결정
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
  */
 
 class MimeTypeTests {
 
-  Path path = Paths.get("src/test/resources/sample.pptx");
-  String expected = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
-
   @Test
-  void tika() throws Exception {
+  void tika_pptx() throws Exception {
+    Path path = Paths.get("src/test/resources/sample.pptx");
+    String expected = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
     Tika tika = new Tika();
     String type = tika.detect(path.toFile());
     assertThat(type).isEqualTo(expected);
@@ -31,10 +31,12 @@ class MimeTypeTests {
   }
 
   @Test
-  void tika_확장자변경한경우_파일내용으로인식함() throws Exception {
+  void tika_pptx_no_extension() throws Exception {
+    Path path = Paths.get("src/test/resources/sample_pptx");
+    String expected = "application/x-tika-ooxml";
     Tika tika = new Tika();
-    String type = tika.detect(Paths.get("src/test/resources/img-png.jpg").toFile());
-    assertThat(type).isEqualTo("image/png");
+    String type = tika.detect(path.toFile());
+    assertThat(type).isEqualTo(expected);
   }
 
   @Test
@@ -44,17 +46,38 @@ class MimeTypeTests {
     assertThat(type).isEqualTo("image/png");
   }
 
-
+  @Test
+  void tika_hwpx() throws Exception {
+    Path path = Paths.get("src/test/resources/sample.hwpx");
+    Tika tika = new Tika();
+    assertThat(tika.detect(path.toFile())).isEqualTo("application/hwp+zip");
+    assertThat(tika.detect(path.toFile().getName())).isEqualTo("application/hwp+zip");
+  }
 
   @Test
-  void usingJava7() throws Exception {
+  void tika_hwpx_no_extension() throws Exception {
+    Path path = Paths.get("src/test/resources/sample_hwpx");
+    Tika tika = new Tika();
+    assertThat(tika.detect(path.toFile())).isEqualTo("application/zip");
+  }
+
+  @Test
+  void probeContentType() throws Exception {
     Path path = Paths.get("src/test/resources/sample.pptx");
+    String expected = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
     String type = Files.probeContentType(path);
     assertThat(type).isEqualTo(expected);
   }
 
   @Test
-  void usingJava7_확장자변경한경우_확장자로인식함() throws Exception {
+  void probeContentType_hwpx_then_null() throws Exception {
+    Path path = Paths.get("src/test/resources/sample.hwpx");
+    String type = Files.probeContentType(path);
+    assertThat(type).isEqualTo(null);
+  }
+
+  @Test
+  void probeContentType_확장자변경한경우_확장자로인식함() throws Exception {
     Path path = Paths.get("src/test/resources/img-png.jpg");
     String type = Files.probeContentType(path);
     assertThat(type).isEqualTo("image/jpeg");
@@ -62,7 +85,7 @@ class MimeTypeTests {
   }
 
   @Test
-  void usingJava7_확장자없는경우_null() throws Exception {
+  void probeContentType_확장자없는경우_null() throws Exception {
     Path path = Paths.get("src/test/resources/img");
     String type = Files.probeContentType(path);
     assertThat(type).isNull();
