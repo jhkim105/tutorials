f8dc80f8149b3b288d730f6443de9aef6c5b5e17
jhkim105 <jhkim105@gmail.com>
2024-03-14 07:34:51 +0900

    refactor:

--- messaging/activemq/spring-activemq-guarantee-order/src/test/java/com/example/demo/SearchControllerIntegrationTest.java
+++ messaging/activemq/spring-activemq-guarantee-order/src/test/java/com/example/demo/SearchControllerIntegrationTest.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
