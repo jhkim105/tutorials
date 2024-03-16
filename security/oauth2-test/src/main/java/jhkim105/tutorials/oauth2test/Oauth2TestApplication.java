483a2e1c27c224ed05db6ec1ec85503cbada42ff
jhkim105 <jhkim105@gmail.com>
2024-03-16 13:03:00 +0900

    .

---
+++ security/oauth2-test/src/main/java/jhkim105/tutorials/oauth2test/Oauth2TestApplication.java
@@ -0,0 +1,13 @@
+package jhkim105.tutorials.oauth2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2TestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Oauth2TestApplication.class, args);
+  }
+
+}
