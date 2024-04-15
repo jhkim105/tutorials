3f4d5e73a4a5e462ae9a3b9887c395cedf09f0c8
jhkim105 <jhkim105@gmail.com>
2024-04-15 15:29:49 +0900

    .

--- persistence/spring-data-dbcp2/src/main/java/jhkim105/tutorials/dbcp2/service/UserManager.java
+++ persistence/spring-data-dbcp2/src/main/java/jhkim105/tutorials/dbcp2/service/UserManager.java
@@ -1,8 +1,8 @@
 package jhkim105.tutorials.dbcp2.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import jhkim105.tutorials.dbcp2.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
