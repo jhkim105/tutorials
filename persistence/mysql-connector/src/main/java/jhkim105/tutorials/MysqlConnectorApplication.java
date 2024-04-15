942336512e9a8bc23705b5e22e9f8eecdbd81a59
jhkim105 <jhkim105@gmail.com>
2024-04-16 07:29:24 +0900

    mysql-connector

---
+++ persistence/mysql-connector/src/main/java/jhkim105/tutorials/MysqlConnectorApplication.java
@@ -0,0 +1,13 @@
+package jhkim105.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MysqlConnectorApplication.class, args);
+  }
+
+}
---
+++ persistence/mysql-connector/src/main/java/jhkim105/tutorials/domain/User.java
@@ -0,0 +1,23 @@
+package jhkim105.tutorials.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "dm_user")
+public class User {
+  @Id
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @GeneratedValue(generator = "uuid")
+  @Column(length = 50)
+  private String id;
+
+  @Column(nullable = false)
+  private String name;
+
+}
