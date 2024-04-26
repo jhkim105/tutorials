2c42140e0d334aa1b870b1fbbee9895014749a9d
jhkim105 <jhkim105@gmail.com>
2024-04-26 18:03:13 +0900

    cors

--- spring-web/spring-cors/src/main/java/jhkim105/tutorials/config/CorsConfig.java
+++ spring-web/spring-cors/src/main/java/jhkim105/tutorials/config/CorsConfig.java
@@ -1,18 +1,29 @@
 package jhkim105.tutorials.config;
 
-import jakarta.servlet.Filter;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
-//@Configuration
+@Configuration
 public class CorsConfig {
 
+
+  // 우선순위를 지정해야 하는 경우
+//  @Bean
+//  public FilterRegistrationBean<CorsFilter> corsFilterBean() {
+//    FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>(corsFilter());
+//    registrationBean.setOrder(0); // 우선순위 높게. 제일 먼저 실행되게 하려면(Ordered.HIGHEST_PRECEDENCE)
+//
+//    return registrationBean;
+//  }
+
+
   @Bean
-  Filter corsFilter() {
+  public CorsFilter corsFilter() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(List.of("*"));
     configuration.setAllowedMethods(List.of("*"));
@@ -22,4 +33,5 @@ public class CorsConfig {
 
     return new CorsFilter(source);
   }
+
 }
