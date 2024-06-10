00a25eb5570f52af57ddcffb2474592b3edb9d7f
jhkim105 <jhkim105@gmail.com>
2024-06-10 14:56:15 +0900

    Batch insert

---
+++ persistence/spring-data-jpa/src/main/java/jhkim105/tutorials/domain/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package jhkim105.tutorials.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+}
