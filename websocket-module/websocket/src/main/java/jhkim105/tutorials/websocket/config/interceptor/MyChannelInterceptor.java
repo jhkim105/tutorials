62e101e06f9906d6959f13be3422e1821b7c3198
jhkim105 <jhkim105@gmail.com>
2024-07-22 21:01:08 +0900

    websocket

--- websocket-module/websocket/src/main/java/jhkim105/tutorials/websocket/config/interceptor/MyChannelInterceptor.java
+++ websocket-module/websocket/src/main/java/jhkim105/tutorials/websocket/config/interceptor/MyChannelInterceptor.java
@@ -1,4 +1,4 @@
-package jhkim105.tutorials.websocket.interceptor;
+package jhkim105.tutorials.websocket.config.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
