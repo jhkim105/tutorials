f4dc95279f585db6b1bfd5db64b13238a725bd40
jhkim105 <jhkim105@gmail.com>
2024-07-22 16:18:51 +0900

    websocket - client test

---
+++ websocket-module/websocket/src/main/java/jhkim105/tutorials/websocket/interceptor/MyChannelInterceptor.java
@@ -0,0 +1,23 @@
+package jhkim105.tutorials.websocket.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+
+
+@Slf4j
+public class MyChannelInterceptor implements ChannelInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        log.debug("message-> {}", message);
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompCommand command = accessor.getCommand();
+        log.debug("accessor-> {}", accessor);
+        log.debug("command-> {}", command);
+        return message;
+    }
+}
