788640463ba048a12ab5096aff3ca5190ba5928a
jhkim105 <jhkim105@gmail.com>
2024-07-24 10:29:22 +0900

    update spring kafka document

--- messaging/kafka/spring-kafka/README.md
+++ messaging/kafka/spring-kafka/README.md
@@ -83,8 +83,15 @@ public DefaultErrorHandler defaultErrorHandler() {
 }
 ```
 
+## Log Compaction
+- cleanup.policy=compact
+- 메시지 전송시 메시지 키를 같이 보내야 한다
+- 각 키에 대한 최신값을 유지, 이전 값은 삭제 (log compaction) -> 공간 절약
+- https://kafka.apache.org/documentation/#compaction
+- ![log-compaction.png](log-compaction.png)
 ## References
 
 - [Spring Boot Kafka](https://docs.spring.io/spring-boot/docs/current/reference/html/messaging.html#messaging.kafka)
 - [Baeldung](https://www.baeldung.com/spring-kafka) 
+- [Apache Kafka](https://kafka.apache.org/documentation/#gettingStarted) 
 
--- messaging/kafka/spring-kafka/src/main/java/jhkim105/tutorials/kafka/config/KafkaConfig.java
+++ messaging/kafka/spring-kafka/src/main/java/jhkim105/tutorials/kafka/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package jhkim105.tutorials.kafka.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
@@ -14,5 +16,15 @@ public class KafkaConfig {
     return new DefaultErrorHandler(new FixedBackOff(0, 2));
   }
 
-
+  @Bean
+  public NewTopic topicFoo() {
+    return TopicBuilder.name(Topics.SAMPLE)
+        .partitions(1)
+        .replicas(1)
+//        .compact() // cleanup.policy=compact
+        .build();
+  }
+  public static class Topics {
+    public static final String SAMPLE = "queue.sample";
+  }
 }
